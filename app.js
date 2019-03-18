c061261f2dad8036d46ce2fdb7249eabbbf33535
edjunma <junwenma@buffalo.edu>
2019-03-17 22:04:45 -0400

    Added get and fetch from item controllers

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function() {
 
   // Public methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
@@ -30,8 +33,28 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemlist: '#item-list';
+  }
+
   // Public methods
-  return {};
+  return {
+    populateItemList: function(items) {
+      let html = "";
+
+      items.forEach(function(item) {
+        html += `li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories}</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemlist).innerHTML = html;
+    }
+  };
 })();
 
 // App Controller
@@ -39,7 +62,11 @@ const App = (function(ItemCtrl, UICtrl) {
   // Public Methods
   return {
     init: function() {
-      console.log("Initiliazing App...");
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   };
 })(ItemCtrl, UICtrl);
